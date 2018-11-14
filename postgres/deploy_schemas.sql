c144fb0a1affccc229f6f8cef56220f00e5688ce
Shijia Hu <shijiahu2016a@gmail.com>
2018-11-14 00:09:01 -0800

    adding JWT authorization

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -3,3 +3,6 @@
 -- run
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
 \i '/docker-entrypoint-initdb.d/tables/login.sql'
+
+-- For testing purposes only. This file will add dummy data
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
